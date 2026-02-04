bf4e6869644a712b0233276304340eee6cab0c09
Justin Ramdin <justinramdin001@gmail.co>
2026-02-04 14:14:55 -0400

    WIP: ...

--- src/Pos.InitializeDb/obj/Debug/net10.0/Pos.InitializeDb.AssemblyInfo.cs
+++ src/Pos.InitializeDb/obj/Debug/net10.0/Pos.InitializeDb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pos.InitializeDb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee2f896877692aa77b26ed3072b129ef8b4e805e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fb9d452435d337f2e595e30e15af4e5b86e4ff0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pos.InitializeDb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pos.InitializeDb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

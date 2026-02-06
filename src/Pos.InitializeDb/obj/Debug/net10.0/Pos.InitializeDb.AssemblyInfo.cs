42961ed93370baee4aa5b48949f2001457e6ee54
Justin Ramdin <justinramdin001@gmail.co>
2026-02-06 14:46:08 -0400

    WIP: ...

--- src/Pos.InitializeDb/obj/Debug/net10.0/Pos.InitializeDb.AssemblyInfo.cs
+++ src/Pos.InitializeDb/obj/Debug/net10.0/Pos.InitializeDb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pos.InitializeDb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1011caa7d26dd5b5d135db412b41974184112d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5944d09671bb29b7479ad8fac069b2ae778bdc89")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pos.InitializeDb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pos.InitializeDb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Pos.Terminal/obj/Debug/net10.0/Pos.Terminal.AssemblyInfo.cs
+++ src/Pos.Terminal/obj/Debug/net10.0/Pos.Terminal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pos.Terminal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1011caa7d26dd5b5d135db412b41974184112d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5944d09671bb29b7479ad8fac069b2ae778bdc89")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pos.Terminal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pos.Terminal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

56cab36e98d2df8df3f0243e11967554f2d06e92
Justin Ramdin <justinramdin001@gmail.co>
2026-04-25 13:14:05 -0400

    WIP: ...

--- src/Pos.Terminal/Views/ReportsView.axaml.cs
+++ src/Pos.Terminal/Views/ReportsView.axaml.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Threading.Tasks;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Markup.Xaml;
 using Avalonia.VisualTree;
 
@@ -18,7 +21,42 @@ public partial class ReportsView : UserControl
         AvaloniaXamlLoader.Load(this);
     }
     
-    public async void OpenExportTemplate_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    public void OpenExportTemplate_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        if (sender is Control control)
+        {
+            FlyoutBase.ShowAttachedFlyout(control);
+        }
+    }
+
+    public async void OpenExportWorkspace_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        if (sender is not Control control || control.Tag is not ExportTemplateDefinition template)
+            return;
+
+        await OpenExportTemplateDialogAsync(template);
+    }
+
+    public async void OpenExportLast7Days_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        vm.FromDate = DateTime.Today.AddDays(-6);
+        vm.ToDate = DateTime.Today;
+
+        await OpenExportTemplateDialogAsync(template);
+    }
+
+    public async void OpenExportThisMonth_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        if (sender is not Control control || control.Tag is not ExportTemplateDefinition template || DataContext is not ReportsViewModel vm)
+            return;
+
+        vm.FromDate = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+        vm.ToDate = DateTime.Today;
+
+        await OpenExportTemplateDialogAsync(template);
+    }
+
+    private async Task OpenExportTemplateDialogAsync(ExportTemplateDefinition template)
     {
         if (DataContext is not ReportsViewModel vm)
             return;
@@ -43,5 +81,5 @@ public partial class ReportsView : UserControl
         };
 
         await dialog.ShowDialog(host);
-    }
+        }
 }
